e5a73400d6a8a76c450ad2df776944395beb0f4c
Kroese <kroese@users.noreply.github.com>
2025-03-03 20:26:16 +0100

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -50,9 +50,6 @@ if [ -z "$mount" ] || [[ "$mount" == "null" ]] || [ ! -d "/data" ]; then
   error "You did not bind the /data folder!" && exit 18
 fi
 
-# Create directories
-mkdir -p "/images"
-
 # Convert Windows paths to Linux path
 if [[ "$mount" == *":\\"* ]]; then
   mount="${mount,,}"
@@ -70,6 +67,9 @@ if [[ "$mount" != "/data" ]]; then
   rm -rf "$mount"
   ln -s /data "$mount"
 fi
+
+# Create directories
+mkdir -p "/images"
 
 trap - ERR
 cd /opt/umbreld
